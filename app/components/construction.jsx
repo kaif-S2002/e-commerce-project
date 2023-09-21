150ad5d0bf26420d968ff3b2937337e59f283e8f
kaif siddiqui <kaifs8998@gmail.com>
2023-09-22 01:00:48 +0530

    Created a next-app

---
+++ app/components/construction.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const construction = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen border border-black bg-gray-950 text-slate-200">
+      <h1 className="text-6xl font-popins mb-10 text-sky-400">
+        <b>Next.JS</b>
+      </h1>
+      <h1 className="text-4xl pb-5">This project is under construction.</h1>
+      <h2 className="text-2xl border-b border-slate-200 pb-2 ">
+        Thankyou for visiting.
+      </h2>
+    </div>
+  );
+};
+
+export default construction;
